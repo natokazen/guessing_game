154ffafee23701f0dcf0855b2ee8f57637e184af
natokazen <natokazen@gmail.com>
2026-09-05 20:20:00 +0300

    Added max guess counter

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,16 @@ use std::cmp::Ordering;
 fn main() {
     println!("\n --------------------- 󱎓 Welcome to Guess the Number --------------------------- ");
     print!(r#"
-          _____                 _             _____              
-         / ___/_ _____ ___ ___ (_)__  ___ _  / ___/__ ___ _  ___ 
+          _____                 _             _____
+         / ___/_ _____ ___ ___ (_)__  ___ _  / ___/__ ___ _  ___
         / (_ / // / -_|_-<(_-</ / _ \/ _ `/ / (_ / _ `/  ' \/ -_)
-        \___/\_,_/\__/___/___/_/_//_/\_, /  \___/\_,_/_/_/_/\__/ 
-                                    /___/                        
+        \___/\_,_/\__/___/___/_/_//_/\_, /  \___/\_,_/_/_/_/\__/
+                                    /___/
         "#);
 
+    const MAX_GUESS :u32 = 7;
     let secret_number = rand::random_range(1..101);
+    let mut counter :u32 = 0;
 
     loop {
 
@@ -26,14 +28,31 @@ fn main() {
 
         let guess :u32 = guess.trim().parse().expect("failed to convert into number");
 
-        match guess.cmp(&secret_number) {
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Less => println!("Too small!"),
-            Ordering::Equal => {
-                println!("You win! 🥳");
-                break;
+        if counter == MAX_GUESS {
+            println!("Game Over!");
+            break;
+
+        } else {
+
+            match guess.cmp(&secret_number) {
+
+                Ordering::Greater => {
+                    println!("Too big!");
+                    counter += 1;
+                }
+
+                Ordering::Less => {
+                    println!("Too small!");
+                    counter +=1;
+                }
+
+                Ordering::Equal => {
+                    println!("You win! 🥳");
+                    break;
+                }
             }
         }
+
     }
 
 }
